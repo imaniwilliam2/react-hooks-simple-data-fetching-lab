891db0850811eca61de8310681b855c0b6a35564
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-02-23 15:19:58 -0500

    all testing passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React from "react";
+import { useEffect, useState } from "react";
+
+function App() {
+
+    const [dogImageUrl, setDogImageUrl] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setDogImageUrl(data.message)
+            setIsLoading(false)
+        })
+        .catch(error => console.error('Error fetching data:', error))
+    }, [])
+
+
+
+    return (
+        <div>
+            {isLoading ? <p>Loading...</p> : <img src={dogImageUrl} alt="A Random Dog" />}
+        </div>
+    )
+}
+
+export default App
